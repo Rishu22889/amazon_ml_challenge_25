d5b8b65445c31e36ebfb1716c8d2134c3387fa16
Rishu <rk1258106@gmail.com>
2026-07-03 14:44:34 +0530

    url updated

--- frontend_web/config.js
+++ frontend_web/config.js
@@ -1,3 +1,3 @@
 // Point this at your deployed Render backend URL (no trailing slash).
 // While developing locally, keep it as http://localhost:5001.
-const BACKEND_URL = "http://localhost:5001";
+const BACKEND_URL = "https://amazon-ml-challenge-25.onrender.com";
